acc40c43cacf1551ce040696a66a5cf6cc3e1899
John Metcalf <digital.wilderness@googlemail.com>
2019-01-03 21:16:19 +0000

    Create sumdiv.c

---
+++ sumdiv.c
@@ -0,0 +1,18 @@
+/* sumdiv
+ * returns the sum of the proper divisors of n
+ */
+
+int sumdiv(int n)
+{
+    int i, sum = 1;
+    for ( i = 2; i * i < n; i++ )
+    {
+        if (n % i == 0)
+        {
+            sum += i + n/i;
+        }
+    }
+    if ( i * i == n )
+        sum += i;
+    return sum;
+}
