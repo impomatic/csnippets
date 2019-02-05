be1a620819c47917a8f10b3a550ad913f310ee85
John Metcalf <digital.wilderness@googlemail.com>
2019-02-05 13:00:03 +0000

    Create modexp.c

---
+++ modexp.c
@@ -0,0 +1,16 @@
+/* modexp - modular exponentiation
+ * returns b^e mod m
+ */
+
+unsigned modexp(unsigned b,unsigned e,unsigned m)
+{
+    unsigned c = 1;
+    while (e)
+    {
+        if (e & 1)
+            c = c * b % m;
+        e >>= 1;
+        b = b * b % m;
+    }
+    return c;
+}
