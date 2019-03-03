3c1fe0d6b73ccde679380e4dc764a9e647f00648
John Metcalf <digital.wilderness@googlemail.com>
2019-03-03 23:37:40 +0000

    Update phi.c

--- phi.c
+++ phi.c
@@ -2,12 +2,12 @@
  * returns the number of integers less than n which are coprime to n
  */
 
-unsigned phi(unsigned n)
+unsigned phi( unsigned n )
 {
     unsigned i, totient = n;
     for ( i = 2; i * i <= n; i++ )
     {
-        if (n % i == 0)
+        if ( n % i == 0 )
         {
             do n /= i;
             while ( n % i == 0 );
