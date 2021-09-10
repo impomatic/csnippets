c0ab3dbc2af3bb758d335d14f27e7b5ae18d0cd2
John Metcalf <digital.wilderness@googlemail.com>
2021-09-11 00:41:03 +0100

    Update gcd_recursive.c

--- gcd_recursive.c
+++ gcd_recursive.c
@@ -2,7 +2,7 @@
  * returns the gcd of a and b. 1 if a and b are relatively prime
  */
 
-int gcd(int a, int b)
+int gcd( int a, int b )
 {
-    return !b ? a : gcd (b, a % b);
+    return !b ? a : gcd( b, a % b );
 }
